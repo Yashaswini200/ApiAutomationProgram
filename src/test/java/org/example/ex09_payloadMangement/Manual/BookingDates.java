7196b63ac0ae36bb4250b23d5b1f5aeec3b17de6
Yashaswini200 <yashusuresh2050@gmail.com>
2025-04-14 19:17:48 +0530

    commit

---
+++ src/test/java/org/example/ex09_payloadMangement/Manual/BookingDates.java
@@ -0,0 +1,22 @@
+package org.example.ex09_payloadMangement.Manual;
+
+public class BookingDates {
+    public String getCheckin() {
+        return checkin;
+    }
+
+    public void setCheckin(String checkin) {
+        this.checkin = checkin;
+    }
+
+    public String getCheckout() {
+        return checkout;
+    }
+
+    public void setCheckout(String checkout) {
+        this.checkout = checkout;
+    }
+
+    private String checkin;
+    private String checkout;
+}
